9efcfcd41ed4640880ed143c7611283a32f85088
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2017-05-22 17:10:41 +0100

    Adding dungeon game file layout

---
+++ dungeon_game.py
@@ -0,0 +1,11 @@
+# draw grid
+# pick random location for player
+# pick random location for door
+# pick random location for monster
+# draw player in the grid
+# take input for movement
+# move player unless beyond grid limits
+# check for win/lose
+# clear screen & redraw grid
+
+
