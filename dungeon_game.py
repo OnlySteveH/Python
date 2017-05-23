d5590f1b1e7e74618ab742440ba83ab4f45031b5
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2017-05-23 14:13:38 +0100

    adding move boundaries - needs a test
    Signed-off-by: OnlySteveH <steve.hunter@bigdogconsultants.co.uk>

--- dungeon_game.py
+++ dungeon_game.py
@@ -7,18 +7,22 @@
 # move player unless beyond grid limits
 # check for win/lose
 # clear screen & redraw grid
+import random
 
-CELLS = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
-		 (0, 1), (1, 1), (2, 1), (3, 1), (4, 1),
-		 (0, 2), (1, 2), (2, 2), (3, 2), (4, 2),
-		 (0, 3), (1, 3), (2, 3), (3, 3), (4, 3),
-		 (0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]
+CELLS =     [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
+		    (0, 1), (1, 1), (2, 1), (3, 1), (4, 1),
+		    (0, 2), (1, 2), (2, 2), (3, 2), (4, 2),
+		    (0, 3), (1, 3), (2, 3), (3, 3), (4, 3),
+		    (0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]
 
 def get_locations():
-	monster = None
-	door = None
-	player = None
-	return monster, door, player
+    monster = None
+    door = None
+    player = None
+    return monster, door, player
+
+def set_locations():
+    monster, door, player = random.sample(CELLS, 3)
 
 def move_player(player, move):
     # get player location
@@ -31,16 +35,24 @@ def move_player(player, move):
 def get_moves(player):
     moves = ["LEFT", "RIGHT", "UP", "DOWN"]
     # if player y == 0, can't move UP
+    if player[1] == 0:
+        moves.remove("UP")
     # if player y == 4, can't move DOWN
+    if player[1] == 4:
+        moves.remove("DOWN")
     # if player x == 0, can't move LEFT
+    if player[0] == 0:
+        moves.remove("LEFT")
     # if player x == 4, can't move RIGHT
+    if player[0] == 4:
+        moves.remove("RIGHT")
     return moves
 
 while True:
-	print("Welcome to the Dungeon!")
-	print("You are currently in room {}") # fill with player position
-	print("You can move {}") # fill with available moves
-	print("Enter QUIT to quit.")
+    print("Welcome to the Dungeon!")
+    print("You are currently in room {}".format(player))  # fill with player position
+    print("You can move {}".format(get_moves(player)))  # fill with available moves
+    print("Enter QUIT to quit.")
 
     move = input("> ")
     move = move.upper()
