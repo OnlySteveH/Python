d8fcb4e112ae502c17a28909363fb2ab59c1fa13
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2017-05-27 14:14:57 +0100

    splitting into multiple files and attributes

--- characters.py
+++ characters.py
@@ -1,24 +1,6 @@
-import random
-
 class Character:
     def __init__(self, name, **kwargs):
         self.name = name
 
         for key, value in kwargs.items():
             setattr(self, key, value)
-
-
-class Thief(Character):
-    sneaky = True
-    # def __init__(self, name, sneaky, **kwargs):
-    #     self.name = name
-    #     self.sneaky = sneaky
-    #
-    #     for key, value in kwargs.items():
-    #         setattr(self, key, value)
-
-    def pickpocket(self):
-        return self.sneaky and bool(random.randint(0, 1))
-
-    def hide(self, light_level):
-        return self.sneaky and light_level < 10
