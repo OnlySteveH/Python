49cac4721759ea52451869380dab0f3b5c96d792
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2017-06-01 15:41:17 +0100

    Update dice.py

--- Yatzy/dice.py
+++ Yatzy/dice.py
@@ -8,3 +8,6 @@ class Die:
             raise ValueError("Sides must be a whole number")
         self.value = value or random.randInt(1, sides)
         
+class D6(Die):
+    def __init__(self, value=0):
+        super().__init__(sides=6, value=value)
