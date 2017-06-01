c8eae6245cea233258eab0ddc6dad0e1ebc8b280
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2017-06-01 17:28:27 +0100

    refining the Yatzy RPG

--- Yatzy/dice.py
+++ Yatzy/dice.py
@@ -4,9 +4,9 @@ class Die:
     def __init__(self, sides=2, value=0):
         if not sides >= 2:
             raise ValueError("Must have at least 2 sides")
-        is not isinstance(sides, int):
+        if not isinstance(sides, int):
             raise ValueError("Sides must be a whole number")
-        self.value = value or random.randInt(1, sides)
+        self.value = value or random.randint(1, sides)
     
     def __int__(self):
         return self.value
@@ -28,6 +28,15 @@ class Die:
     
     def __le__(self, other):
         return int(self) < other or int(self) == other
+
+    def __add__(self, other):
+        return int(self) + other
+
+    def __radd__(self, other):
+        return int(self) + other
+
+    def __repr__(self):
+        return str(self.value)
 
     
 class D6(Die):
