5d6d2aa540f542e0341f6f5b439a33edfd3c5e51
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2017-06-01 15:47:55 +0100

    adding overridden operator methods

--- Yatzy/dice.py
+++ Yatzy/dice.py
@@ -7,7 +7,29 @@ class Die:
         is not isinstance(sides, int):
             raise ValueError("Sides must be a whole number")
         self.value = value or random.randInt(1, sides)
-        
+    
+    def __int__(self):
+        return self.value
+    
+    def __eq__(self, other):
+        return int(self) == other
+    
+    def __ne__(self, other):
+        return not int(self) == other
+    
+    def __gt__(self, other):
+        return int(self) > other
+    
+    def __lt__(self, other):
+        return int(self) < other
+    
+    def __ge__(self, other):
+        return int(self) > other or int(self) == other
+    
+    def __le__(self, other):
+        return int(self) < other or int(self) == other
+
+    
 class D6(Die):
     def __init__(self, value=0):
         super().__init__(sides=6, value=value)
